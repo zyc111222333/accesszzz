4a3701a3ae4a738c4ff88b0efac2ca3de29f8b8f
zyc111222333 <113100642+zyc111222333@users.noreply.github.com>
2022-10-31 20:51:24 +0800

    Add files via upload

---
+++ detect.js
@@ -0,0 +1,10 @@
+/*
+let ua = navigator.userAgent
+let reg = /LE210\d|OnePlus 9R/im
+if(reg.test(ua)){
+    alert("一加9R与狗不得入内")
+    window.open('','_self')
+    window.close()
+    window.location.href = "http://example.com/"
+}
+*/
